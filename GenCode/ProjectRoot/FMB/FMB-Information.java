05b0a381d33cbdf64790553a5f0f118e0d4739dd
Tibell <rasmus.tibell@socialstyrelsen.se>
2026-09-23 14:02:26 +0200

    add references to FMB

---
+++ GenCode/ProjectRoot/FMB/FMB-Information.java
@@ -0,0 +1,23 @@
+package ProjectRoot.FMB;
+
+import ProjectRoot.Common.Metadata;
+
+/**
+ * @author rstl01
+ * @version 1.0
+ * @created 23-sep-2026 13:57:38
+ */
+public class FMB-Information {
+
+	public Metadata meta;
+	public Referens ref;
+
+	public FMB-Information(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+
+}
